7f54ee11c906433ea76bd0db6d8585787e5ae32f
Ioann Mel'nikov <151534349+ellerotta@users.noreply.github.com>
2026-08-19 16:06:05 +0300

    ANDROID: block/fscrypt/f2fs: plumb bi_skip_dm_default_key flag
    Core plumbing required by dm-default-key: a per-bio flag that lets
    fscrypt-encrypted regular files skip the dm-default-key layer (their
    contents are already encrypted by fscrypt, so the default key must not
    be applied on top).
    
    Ported from AOSP common kernel android-mainline @ db2acc01b75f9a30bfef0f4d2a6f413542cc32ac:
      include/linux/blk_types.h: bi_skip_dm_default_key in struct bio
      include/linux/blk-crypto.h: set/should_skip/clone helpers + clone hook
      include/linux/fscrypt.h: fscrypt_inode_should_skip_dm_default_key()
      fs/crypto/inline_crypt.c, fs/f2fs/data.c: set/mergeable call sites
    
    Fixes: 4995f6232 ("ANDROID: dm: add dm-default-key target")

--- include/linux/blk-crypto.h
+++ include/linux/blk-crypto.h
@@ -204,6 +204,38 @@ static inline void blk_crypto_submit_bio(struct bio *bio)
 }
 
 int __bio_crypt_clone(struct bio *dst, struct bio *src, gfp_t gfp_mask);
+
+#if IS_ENABLED(CONFIG_DM_DEFAULT_KEY)
+static inline void bio_set_skip_dm_default_key(struct bio *bio)
+{
+	bio->bi_skip_dm_default_key = true;
+}
+
+static inline bool bio_should_skip_dm_default_key(const struct bio *bio)
+{
+	return bio->bi_skip_dm_default_key;
+}
+
+static inline void bio_clone_skip_dm_default_key(struct bio *dst,
+						 const struct bio *src)
+{
+	dst->bi_skip_dm_default_key = src->bi_skip_dm_default_key;
+}
+#else /* CONFIG_DM_DEFAULT_KEY */
+static inline void bio_set_skip_dm_default_key(struct bio *bio)
+{
+}
+
+static inline bool bio_should_skip_dm_default_key(const struct bio *bio)
+{
+	return false;
+}
+
+static inline void bio_clone_skip_dm_default_key(struct bio *dst,
+						 const struct bio *src)
+{
+}
+#endif /* !CONFIG_DM_DEFAULT_KEY */
 /**
  * bio_crypt_clone - clone bio encryption context
  * @dst: destination bio
@@ -218,6 +250,7 @@ int __bio_crypt_clone(struct bio *dst, struct bio *src, gfp_t gfp_mask);
 static inline int bio_crypt_clone(struct bio *dst, struct bio *src,
 				  gfp_t gfp_mask)
 {
+	bio_clone_skip_dm_default_key(dst, src);
 	if (bio_has_crypt_ctx(src))
 		return __bio_crypt_clone(dst, src, gfp_mask);
 	return 0;
